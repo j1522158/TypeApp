54c1e7b33a9efba2e7428c8b0658aa5dd0bbd082
j1522158 <j1522158vu@gmail.com>
2024-05-29 00:55:01 +0900

    サイドバーの移植

---
+++ src/components/common/SideBar.tsx
@@ -0,0 +1,88 @@
+import { Divider, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar } from '@mui/material'
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import { Box } from '@mui/system'
+import React from 'react'
+
+{/* 型定義を一つにまとめられる */}
+interface SideBarProps { 
+    drawerWidth: number,
+    mobileOpen: boolean,
+    handleDrawerToggle: () => void
+}
+
+// これでもいい。定義できる型が多いらしい
+// type SideBarProps = { 
+//     drawerWidth: number,
+//     mobileOpen: boolean,
+//     handleDrawerToggle: () => void
+// }
+
+const SideBar = ({drawerWidth, mobileOpen, handleDrawerToggle}: SideBarProps) => {
+    const drawer = (
+        <div>
+          <Toolbar />
+          <Divider /> {/* 横線 */}
+          <List> {/* サイドバーの項目リスト */}
+            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            {['All mail', 'Trash', 'Spam'].map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </div>
+      );
+
+  return (
+    <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
+      >
+        <Drawer
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: 'none', sm: 'block' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+          open
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+  )
+}
+
+export default SideBar
