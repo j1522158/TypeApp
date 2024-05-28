d91048f93f15096811adfa684e034017afc17a70
j1522158 <j1522158vu@gmail.com>
2024-05-28 23:41:33 +0900

    ルーティングの基礎構築

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import NoMatch from './pages/NoMatch';
+import Report from './pages/Report';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/report" element={<Report />}/>
+        <Route path="*" element={<NoMatch />}/>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/NoMatch.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ページなし = () => {
+  return (
+    <div>ページなし</div>
+  )
+}
+
+export default ページなし
