0d224a192dcab8f0e7ba682d62f67f4c491f8863
j1522158 <j1522158vu@gmail.com>
2024-05-29 19:52:52 +0900

    Homeのコンポーネント作成

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,24 @@
+import { Box } from '@mui/material'
 import React from 'react'
+import Calender from '../components/Calender'
+import MonthlySummary from '../components/MonthlySummary'
+import TransactionForm from '../components/TransactionForm'
+import TransactionMenu from '../components/TransactionMenu'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box>
+      {/* 左 */}
+      <Box>
+        <MonthlySummary />
+        <Calender />
+      </Box>
+      {/* 右 */}
+      <Box>
+        <TransactionMenu />
+        <TransactionForm />
+      </Box>
+    </Box>
   )
 }
 
