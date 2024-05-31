cfaac1b024c4eaeb6ec95895b9323cebedf17c0a
j1522158 <j1522158vu@gmail.com>
2024-06-01 02:52:21 +0900

    カードの調整

--- src/components/MonthlySummary.tsx
+++ src/components/MonthlySummary.tsx
@@ -7,13 +7,72 @@ import { fontSize } from '@mui/system';
 
 const MonthlySummary = () => {
   return (
-    <Grid container> {/* container: 要素が横並びになる */}
-      <Grid item>
-        <Card sx={{bgcolor: 'blue', color: 'white', borderRadius: '10px'}}>
+    <Grid container spacing={{xs: 1, sm: 2}} mb={2}> {/* container: 要素が横並びになる */}
+      {/* 収入 */}
+      <Grid item xs={4} display={"flex"} flexDirection={"column"}>
+        <Card 
+          sx={{
+            bgcolor: 'blue', 
+            color: 'white', 
+            borderRadius: '10px',
+            flexGrow: 1
+          }}>
           <CardContent sx={{padding: {xs: 1, sm: 2}}}>
             <Stack direction={"row"}>
               <CurrencyYenIcon sx={{fontSize: '2rem'}}/>
               <Typography>収入</Typography>
+            </Stack>
+            <Typography 
+            textAlign={"right"} 
+            variant={"h5"} 
+            fontWeight={"fontWeigthBold"}
+            sx={{
+              wordBreak: "break-word", 
+              fontSize: { xs: ".8rem", sm: "1rem", md: "1.2rem" },
+          }}
+            >¥300</Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+      {/* 支出 */}
+      <Grid item xs={4} display={"flex"} flexDirection={"column"}>
+        <Card 
+          sx={{
+            bgcolor: 'red', 
+            color: 'white', 
+            borderRadius: '10px', 
+            flexGrow: 1
+          }}>
+          <CardContent sx={{padding: {xs: 1, sm: 2}}}>
+            <Stack direction={"row"}>
+              <ShoppingCartCheckoutIcon sx={{fontSize: '2rem'}}/>
+              <Typography>支出</Typography>
+            </Stack>
+            <Typography 
+            textAlign={"right"} 
+            variant={"h5"} 
+            fontWeight={"fontWeigthBold"}
+            sx={{
+              wordBreak: "break-word", 
+              fontSize: { xs: ".8rem", sm: "1rem", md: "1.2rem" },
+          }}
+            >¥300</Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+      {/* 残高 */}
+      <Grid item xs={4} display={"flex"} flexDirection={"column"}>
+        <Card 
+          sx={{
+            bgcolor: 'green', 
+            color: 'white', 
+            borderRadius: '10px',
+            flexGrow: 1
+            }}>
+          <CardContent sx={{padding: {xs: 1, sm: 2}}}>
+            <Stack direction={"row"}>
+              <AccountBalanceIcon sx={{fontSize: '2rem'}}/>
+              <Typography>残高</Typography>
             </Stack>
             <Typography 
             textAlign={"right"} 
