703741f0dd513539521ef7d8cd3cc0b637cc1ac0
j1522158 <j1522158vu@gmail.com>
2024-05-31 23:32:29 +0900

    サイズ変更

--- src/components/MonthlySummary.tsx
+++ src/components/MonthlySummary.tsx
@@ -10,7 +10,7 @@ const MonthlySummary = () => {
     <Grid container> {/* container: 要素が横並びになる */}
       <Grid item>
         <Card sx={{bgcolor: 'blue', color: 'white', borderRadius: '10px'}}>
-          <CardContent>
+          <CardContent sx={{padding: {xs: 1, sm: 2}}}>
             <Stack direction={"row"}>
               <CurrencyYenIcon sx={{fontSize: '2rem'}}/>
               <Typography>収入</Typography>
@@ -19,7 +19,10 @@ const MonthlySummary = () => {
             textAlign={"right"} 
             variant={"h5"} 
             fontWeight={"fontWeigthBold"}
-            sx={{wordBreak: "break-word"}}
+            sx={{
+              wordBreak: "break-word", 
+              fontSize: { xs: ".8rem", sm: "1rem", md: "1.2rem" },
+          }}
             >¥300</Typography>
           </CardContent>
         </Card>
