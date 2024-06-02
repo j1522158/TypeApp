67af1e1ee0f47e494cd43c605f06eeb0d56aa1fb
j1522158 <j1522158vu@gmail.com>
2024-06-02 15:07:55 +0900

    カレンダーcss追加

--- src/components/Calender.tsx
+++ src/components/Calender.tsx
@@ -2,6 +2,7 @@ import FullCalendar from '@fullcalendar/react'
 import React from 'react'
 import dayGridPlugin from "@fullcalendar/daygrid"
 import jaLocale from "@fullcalendar/core/locales/ja"
+import "../calender.css"
 
 const Calender = () => {
   return (
