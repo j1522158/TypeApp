b6159e26f96b8c2782b7b4c25bf54d07efaa9915
j1522158 <j1522158vu@gmail.com>
2024-06-03 00:53:27 +0900

    日付ごとの収支計算の処理追加

--- src/utils/financeCalculations.ts
+++ src/utils/financeCalculations.ts
@@ -1,3 +1,4 @@
+import { dA } from "@fullcalendar/core/internal-common";
 import { Balance, Transaction } from "../types";
 
 export function financeCalculations(transactions: Transaction[]): Balance {
@@ -10,4 +11,21 @@ export function financeCalculations(transactions: Transaction[]): Balance {
         acc.balance += acc.income - acc.expense;
         return acc
     }, {income: 0, expense: 0, balance: 0})
+}
+
+// 日付ごとの収支を計算する関数
+export function calculateDailyBalances(transactions: Transaction[]): Record<string, Balance>{
+    return transactions.reduce<Record<string, Balance>>((acc, transaction) => {
+        const day = transaction.date;
+        if(!acc[day]){
+            acc.day = {income: 0, expense: 0, balance: 0}
+        }
+        if(transaction.type === "income") {
+            acc[day].income += transaction.amount
+        } else {
+            acc[day].expense += transaction.amount
+        }
+        acc[day].balance = acc[day].income - acc[day].expense
+        return acc
+    }, {})
 }
